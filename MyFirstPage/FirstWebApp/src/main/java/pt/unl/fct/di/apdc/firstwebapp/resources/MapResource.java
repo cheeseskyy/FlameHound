adc80503829bd40518e0af4dffe48f6d6ca26cf2
gpslopes <gp.lopes@campus.fct.unl.pt>
2018-03-28 21:48:33 +0100

    Nova versao

---
+++ MyFirstPage/FirstWebApp/src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/MapResource.java
@@ -0,0 +1,21 @@
+package pt.unl.fct.di.apdc.firstwebapp.resources;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MapResource extends HttpServlet{
+	
+	public MapResource() {}
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException , ServletException{
+		RequestDispatcher r = request.getRequestDispatcher("pages/homePage.html");
+		r.forward(request, response);
+	}
+
+}
