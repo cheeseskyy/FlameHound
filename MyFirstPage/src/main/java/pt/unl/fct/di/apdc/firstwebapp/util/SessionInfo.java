3f0032a1f31bd35b15ef1e77557ceafafade1e58
gpslopes <gp.lopes@campus.fct.unl.pt>
2018-04-26 14:15:29 +0100

    hfjgkjkk

--- MyFirstPage/src/main/java/pt/unl/fct/di/apdc/firstwebapp/util/SessionInfo.java
+++ MyFirstPage/src/main/java/pt/unl/fct/di/apdc/firstwebapp/util/SessionInfo.java
@@ -1,9 +1,14 @@
 package pt.unl.fct.di.apdc.firstwebapp.util;
 
+import java.util.List;
+
+import com.google.appengine.api.datastore.DatastoreService;
+
 public class SessionInfo {
 	
 	public String username;
 	public String tokenId;
+	private List<Object> arguments;
 	
 	public SessionInfo() {
 	}
@@ -11,5 +16,13 @@ public class SessionInfo {
 	public SessionInfo(String username, String tokenId) {
 		this.username = username;
 		this.tokenId = tokenId;
+	}
+	
+	public void setArgument(Object object) {
+		arguments.add(object);
+	}
+
+	public List<Object> getArgs() {
+		return arguments;
 	}
 }
