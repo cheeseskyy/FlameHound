5e88b19cd1223772c43f141229cf43e284235ba6
gpslopes <gp.lopes@campus.fct.unl.pt>
2018-04-26 13:39:43 +0100

    dfghjkl

---
+++ MyFirstPage/src/main/java/pt/unl/fct/di/apdc/firstwebapp/util/Coordinates.java
@@ -0,0 +1,37 @@
+package pt.unl.fct.di.apdc.firstwebapp.util;
+
+public class Coordinates {
+
+	private long x;
+	private long y;
+	
+	
+	public Coordinates() {
+	}
+	
+	public Coordinates(long x, long y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public long getX() {
+		return x;
+	}
+
+	public void setX(long x) {
+		this.x = x;
+	}
+
+	public long getY() {
+		return y;
+	}
+
+	public void setY(long y) {
+		this.y = y;
+	}
+	
+	public String coordToString() {
+		return "(" + x + "," + y + ")";
+	}
+	
+}
