4d24f9623e6284f5fde8dbaff2af7c9ce9fe7fd5
hvalee <henriquevale99@gmail.com>
2018-05-13 16:02:31 +0100

    going to give a better look to the description of the ocorrency
    maps ready to be worked on
    
    (upload image implemented)
    
    still alot to do.

--- app/src/main/java/com/example/hvale/loginapp/RegistarOcorrencia.java
+++ app/src/main/java/com/example/hvale/loginapp/RegistarOcorrencia.java
@@ -1,17 +1,23 @@
 package com.example.hvale.loginapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 import static android.R.layout.simple_spinner_item;
 
-public class RegistarOcorrencia extends AppCompatActivity {
+public class RegistarOcorrencia extends AppCompatActivity implements View.OnClickListener {
+    private static final int RESULT_LOAD_IMAGE = 1;
+    ImageView imageToUpload;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,11 @@ public class RegistarOcorrencia extends AppCompatActivity {
             }
         });
 
+        imageToUpload = (ImageView) findViewById(R.id.imageToUpload);
+        imageToUpload.setOnClickListener(this);
+
+
+        //Spinner Content (put in a method after)
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
 
     // Create an ArrayAdapter using the string array and a default spinner layout
@@ -38,5 +49,26 @@ public class RegistarOcorrencia extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
     // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.imageToUpload:
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent,RESULT_LOAD_IMAGE);
+
+                break;
+            //and other listener events (the info)
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode, data);
+        if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            imageToUpload.setImageURI(selectedImage);
+        }
     }
 }
