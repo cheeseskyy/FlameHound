1c14ca798c403d9d0c6d4c382985b837300ff474
Pedro Pais <ppais95@gmail.com>
2018-05-18 09:29:14 +0100

    improved occurrence submit form

--- src/SubmitOccurrence.js
+++ src/SubmitOccurrence.js
@@ -1,13 +1,43 @@
 import React, {Component} from 'react';
 
 class SubmitOcccurrence extends Component{
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                <h1>Placeholder for a submit form</h1>
-            </div>
-        )
-    }
-}
+                <p>Please fill in the form below to submit an occurrence</p>
 
-export default SubmitOcccurrence;
+                <label for="title">
+                    <b>Title</b>
+                </label>
+                <input type="text" placeholder="Enter a title" id="title" required/>
+                    <br/>
+
+                        <label for="description">
+                            <b>Description</b>
+                        </label>
+                        <input type="text" placeholder="Describe the occurrence" id="description" required/>
+                            <br/>
+
+                                <label for="location">
+                                    <b>Location</b>
+                                </label>
+                                <input type="text" placeholder="Enter the location" id="location" required/>
+                                    <br/>
+
+                                        <label for="type">
+                                            <b>Location</b>
+                                        </label>
+                                        <input type="text" placeholder="Enter the type" id="type" required/>
+                                            <br/>
+
+                                                <input type="file" placeholder="Submit an image" id="image" required/>
+                                                    <br/>
+
+                                                        <button type="button" onclick="saveOc()">Submit</button>
+                                                        <br/>
+            </div>
+    )
+    }
+    }
+
+    export default SubmitOcccurrence;
