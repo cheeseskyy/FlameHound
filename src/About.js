b1739cc9bc275477c934183e53fc6b5c77b6a2ce
tania <tania.antunes21@gmail.com>
2018-05-23 16:21:15 +0100

    Texto não mexe xD yeah!!!!

--- src/About.js
+++ src/About.js
@@ -5,26 +5,28 @@ import pick1 from "./imagens/colegas.png";
 class About extends Component{
 
     render(){
-
-        const style = {
-            'text-align': 'center',
-            width: '84vw'
-        }
         return(
         <Router>
-            <div style={style} className="Column">
-                <h1>FlameHound</h1>
+            <div className="Column">
+                <h1 align="center" >FlameHound</h1>
                 <br/>
 
-                <div id ="text">
-                    <p> Somos uma equipa que ao longo de vários meses, aceitou o desafio de desafio de desenvolver um site web com uma componente móvel para indicação das zonas a limpar com uma forte componente social.</p>
-                    <p> Todos nós sabemos como os incêndios assolaram Portugal no ano de 2017, com um registo mortal enorme, nunca antes visto, neste tipo de ocorrências.</p>
-                    <p> Como tal, o nosso objectivo é erradicar estes acontecimentos, nunca os esquecendo mas sim utilizando-os como motor para o desenvolvimento deste projecto.</p>
-                    <p> Uma actuação rápida, sem burocracia, e de cómodo uso.</p>
-                    <p> Deste modo promovemos uma nova ferramenta que entidades podem utilizar ou mesmo, no futuro, promover.</p>
+                <div id="text">
+                    <p> Somos uma equipa que ao longo de vários meses, aceitou o desafio de desafio de desenvolver um
+                        site web com uma componente móvel para indicação das zonas a limpar com uma forte componente
+                        social.</p>
+                    <p> Todos nós sabemos como os incêndios assolaram Portugal no ano de 2017, com um registo mortal
+                        enorme, nunca antes visto, neste tipo de ocorrências.</p>
+                    <p> Como tal, o nosso objectivo é erradicar estes acontecimentos, nunca os esquecendo mas sim
+                        utilizando-os como motor para o desenvolvimento deste projecto. Uma actuação rápida, sem
+                        burocracia, e de cómodo uso.</p>
+                    <p> Deste modo promovemos uma nova ferramenta que entidades podem utilizar ou mesmo, no futuro,
+                        promover.</p>
                 </div>
-                <br/>
-                <img src={pick1}/>
+                <br/><br/><br/>
+                <div align="center">
+                    <img src={pick1}/>
+                </div>
             </div>
         </Router>
 
--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,17 @@
 import React, {Component} from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
 
 class Home extends Component{
 
     render(){
-        const style = {
-            width: '84%',
 
-
-        };
       return(
 
-          <div style={style} className="Column">
-              <div align="center">
-                  <h1 align="center">FlameHound</h1>
-                  <br/>
-              </div>
+          <div className="Column">
+
+              <h1 align="center">FlameHound</h1>
               <br/>
-              <div style={style} id="text">
+
+              <div id="text">
                   <ul>​​​​​​A nossa missão é facilitar a <b>Prevenção de Incêndios</b>, ajudar o país e as pessoas e
                       contribuir para que o sistema funcione melhor para todos nós.
                   </ul>
@@ -32,7 +25,7 @@ class Home extends Component{
               </div>
               <br/>
 
-              <h2 id="title1">Com a sua ajuda, conseguimos farejar os fogos e desta forma prevenir!</h2>
+              <h2 id="title">Com a sua ajuda, conseguimos farejar os fogos e desta forma prevenir!</h2>
               <br/>
           </div>
 
