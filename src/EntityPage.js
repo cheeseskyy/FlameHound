b35b21f507de01be7104638d2bc64d16b9998d48
Pedro Pais <ppais95@gmail.com>
2018-06-06 20:54:13 +0100

    Started work on the EntityPage

--- src/EntityPage.js
+++ src/EntityPage.js
@@ -6,14 +6,17 @@ class EntityPage extends Component{
     render(){
         return(
           <div className={"EntityPage"}>
-              <div id = "coverImage">
-                  <img src={this.props.coverImage} alt = {this.props.name + " cover image"}/>
-              </div>
-              <div id = "nameTitle">
-                  {this.props.name}
-              </div>
-              <div id = "image">
-                  <img src={this.props.logo} alt = {this.props.name + " logo"} />
+              {/* div separada para titulo e assim. para se alinhar tudo*/}
+              <div id= "title">
+                  <div id = "coverImage">
+                      <img src={this.props.coverImage} alt = {this.props.name + "'s cover image"}/>
+                  </div>
+                  <div id = "entityLogo">
+                      <img src={this.props.logo} alt = {this.props.name + "'s logo"} />
+                  </div>
+                  <div id = "nameTitle">
+                      {this.props.name}
+                  </div>
               </div>
               <div id = "statistics">
 
