8e87eb18adf4847ae8f3e664cd84d0b32573f431
Pedro Pais <ppais95@gmail.com>
2018-06-20 16:43:08 +0100

    Created links between pages

--- src/Body.js
+++ src/Body.js
@@ -123,7 +123,7 @@ class Body extends Component {
                     <Route path="/EntityPage" render={(props) =>
                         <EntityPage name = "Placeholder" description = "Placeholder is a company that holds places"/>
                     }/>
-                    <Route path="/PerfilPage" render={(props) =>
+                    <Route path="/profile" render={(props) =>
                         <PerfilPage name = "Placeholder" />
                     }/>
                     <Route path="/Occurrence" render={(props) =>
