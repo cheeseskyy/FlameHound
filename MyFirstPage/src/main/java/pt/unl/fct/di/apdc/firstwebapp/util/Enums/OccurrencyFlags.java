0c64c21437c9074f3662687dd42bf61748b9b2bd
Cheese <pg.pais@campus.fct.unl.pt>
2018-07-03 16:11:46 +0100

    Deleted something that was extra

--- MyFirstPage/src/main/java/pt/unl/fct/di/apdc/firstwebapp/util/Enums/OccurrencyFlags.java
+++
@@ -1,5 +0,0 @@
-package pt.unl.fct.di.apdc.firstwebapp.util.Enums;
-
-public enum OccurrencyFlags {
-	unconfirmed, confirmed, solving, solved
-}
--- MyFirstPage/src/main/java/pt/unl/fct/di/apdc/firstwebapp/util/Enums/UserRoles.java
+++
@@ -1,5 +0,0 @@
-package pt.unl.fct.di.apdc.firstwebapp.util.Enums;
-
-public enum UserRoles {
-	USER, MODERATOR, ADMIN, WORKER
-}
